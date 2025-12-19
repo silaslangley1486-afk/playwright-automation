426f774e635ee3eee34102ed153700ffac6c7999
silaslangley98 <silaslangley98@gmail.com>
2025-12-19 11:32:53 -0800

    refactor: move axe test into assertion function

--- playwright/tests/a11y/login.a11y.spec.ts
+++ playwright/tests/a11y/login.a11y.spec.ts
@@ -1,17 +1,9 @@
-// tests/a11y/login.a11y.spec.ts
-//import { test, expect } from "@playwright/test";
-import { test, expect } from "../../fixtures/auth.fixture.js";
-// import { axeFor } from "./axe";
-import { routes } from "../../constants/routes";
+import { test } from "../../fixtures/auth.fixture.js";
 import { expectNoSeriousA11yViolations } from "./asssertions";
 
 test("@a11y @smoke login page has no serious a11y violations", async ({
   loginPage,
   page,
 }) => {
-  //await page.goto(routes.login);
-
-  await expect(page).toHaveURL(routes.login);
-
   await expectNoSeriousA11yViolations(page);
 });
