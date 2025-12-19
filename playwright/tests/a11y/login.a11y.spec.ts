22d9d26b5109fde8e521bab2f7f5294de228cb29
silaslangley98 <silaslangley98@gmail.com>
2025-12-19 11:32:43 -0800

    refactor: move axe test into assertion function

--- playwright/tests/a11y/login.a11y.spec.ts
+++ playwright/tests/a11y/login.a11y.spec.ts
@@ -1,21 +1,17 @@
 // tests/a11y/login.a11y.spec.ts
-import { test, expect } from "@playwright/test";
-import { axeFor } from "./axe";
-import { routes } from "../../constants/routes.js";
+//import { test, expect } from "@playwright/test";
+import { test, expect } from "../../fixtures/auth.fixture.js";
+// import { axeFor } from "./axe";
+import { routes } from "../../constants/routes";
+import { expectNoSeriousA11yViolations } from "./asssertions";
 
-test("@a11y @smoke login page has no serious violations", async ({ page }) => {
-  await page.goto(routes.login);
+test("@a11y @smoke login page has no serious a11y violations", async ({
+  loginPage,
+  page,
+}) => {
+  //await page.goto(routes.login);
 
-  const { violations } = await axeFor(page).analyze();
+  await expect(page).toHaveURL(routes.login);
 
-  const serious = violations.filter(
-    (violation) =>
-      violation.impact === "serious" || violation.impact === "critical"
-  );
-
-  if (serious.length) {
-    console.log(JSON.stringify(serious, null, 2));
-  }
-
-  expect(serious).toEqual([]);
+  await expectNoSeriousA11yViolations(page);
 });
